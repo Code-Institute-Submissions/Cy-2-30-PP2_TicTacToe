462386321f9a5a13627db61f1d4303fce83402df
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2023-03-26 22:54:00 +0000

    add reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,11 +50,61 @@ window.addEventListener('DOMContentLoaded', () => {
         [4, 9, 14, 19, 24],
         [0, 6, 12, 18, 24],
         [4, 8, 12, 16, 20]
-    ];*/
+    ]; 
+     function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winningCondition = winningCondition[i];
+            const a = board[winningCondition[0]];
+            const b = board[winningCondition[1]];
+            const c = board[winningCondition[2]];
+            const d = board[winningCondition[3]];
+            const e = board[winningCondition[4]];
+            if (a === '' || b === '' || c === '' || d=== '' || e ==='') {
+                continue;
+            }
+            if (a === b && b === c && c === d && d === e ) {
+                roundWon = true;
+                break;
+            }
+        }
+        if (roundWon) {
+            arletor(currentPlayer === 'X' ? PLAYER1_WON : PLAYER2_WON);
+            isGameActive = false;
+            return;
+        }
+        if (!board.includes(''))
+            arletor(TIE);
+    }
 
 
-    
-    
+    */
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winningCondition = winningCondition[i];
+            const a = board[winningCondition[0]];
+            const b = board[winningCondition[1]];
+            const c = board[winningCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+        if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYER1_WON : PLAYER2_WON);
+            isGameActive = false;
+            return;
+        }
+        if (!board.includes(''))
+            arlertor(TIE);
+    }
+ 
+
     tiles.forEach((tile, index) => {
         tile.addEventListener('click', () => userAction(tile, index));
     });
