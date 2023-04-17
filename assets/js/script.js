5f13f595b31de91e336472f5d7ea35edadf6e878
Cy-2-30 <lebulicynthia@gmail.com>
2023-04-17 16:28:31 +0100

    add responsive inline menu to contact.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,90 @@
+const cells = Array.from(document.querySelectorAll('.tile'));
+const popUp = document.querySelector('.popup');
+const resetButton = document.querySelector('#reset');
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-/********** DROP DOWN ON HOVER CLICK! **************/
-function beforeGameOpen(){
+let choiceCells = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = "X";
+let isGameActive = true;
 
+/********** START GAME ON MOUSE CLICK! **************/
+initializeGame();
+
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    resetButton.addEventListener("click", restartGame);
+    popUp.textContent = `${currentPlayer}'s turn`;
+    isGameActive = true;
 }
 
+/************* SHOW CLICKS ON THE BOARD AND CHECK WINNING **************/
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
 
-
-
-const cells = Array.from(document.querySelectorAll('.tile'));
-    const popUp = document.querySelector('.popup');
-    const resetButton = document.querySelector('#reset');
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-        ];
-    
-    let choiceCells = ['', '', '', '', '', '', '', '', ''];
-    let currentPlayer = "X";
-    let isGameActive = true;
-
-    /********** START GAME ON MOUSE CLICK! **************/
-    initializeGame();
-
-    function initializeGame(){
-        cells.forEach(cell => cell.addEventListener("click", cellClicked));
-        resetButton.addEventListener("click", restartGame);
-        popUp.textContent = `${currentPlayer}'s turn`;
-        isGameActive = true;
-    }
-    
-    /************* SHOW CLICKS ON THE BOARD AND CHECK WINNING **************/
-    function cellClicked(){
-        const cellIndex = this.getAttribute("cellIndex");
-    
-        if(choiceCells[cellIndex] != "" || !isGameActive){
-            return;
-        }
-    
-        updateCell(this, cellIndex);
-        checkWinner();
+    if (choiceCells[cellIndex] != "" || !isGameActive) {
+        return;
     }
 
-    /************* UPDATE THE CELL ON THE ACTIVE PLAYER **************/
-    function updateCell(cell, index){
-        choiceCells[index] = currentPlayer;
-        cell.textContent = currentPlayer;
-    }
+    updateCell(this, cellIndex);
+    checkWinner();
+}
 
-    /************** SHOW MESSAGE DISPLAYING PLAYER'S TURNS ****************/
-    function changePlayer(){
-        currentPlayer = (currentPlayer == "X") ? "O" : "X";
-        popUp.textContent = `${currentPlayer}'s turn`;
-    } 
+/************* UPDATE THE CELL ON THE ACTIVE PLAYER **************/
+function updateCell(cell, index) {
+    choiceCells[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
 
-    /*************** AFTER CHECKING FOR A WINNER OR DRAW, STOP THE GAME IF THERE IS ANY ***************/
-    function checkWinner(){
-        let roundWon = false;
-    
-        for(let i = 0; i < winConditions.length; i++){
-            const condition = winConditions[i];
-            const cellA = choiceCells[condition[0]];
-            const cellB = choiceCells[condition[1]];
-            const cellC = choiceCells[condition[2]];
-    
-            if(cellA == "" || cellB == "" || cellC == ""){
-                continue;
-            }
-            if(cellA == cellB && cellB == cellC){
-                roundWon = true;
-                break;
-            }
+/************** SHOW MESSAGE DISPLAYING PLAYER'S TURNS ****************/
+function changePlayer() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    popUp.textContent = `${currentPlayer}'s turn`;
+}
+
+/*************** AFTER CHECKING FOR A WINNER OR DRAW, STOP THE GAME IF THERE IS ANY ***************/
+function checkWinner() {
+    let roundWon = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = choiceCells[condition[0]];
+        const cellB = choiceCells[condition[1]];
+        const cellC = choiceCells[condition[2]];
+
+        if (cellA == "" || cellB == "" || cellC == "") {
+            continue;
         }
-    
-        if(roundWon){
-            popUp.textContent = `${currentPlayer} wins!`;
-            isGameActive = false;
-        }
-        else if(!choiceCells.includes("")){
-            popUp.textContent = `Draw!`;
-            isGameActive = false;
-        }
-        else{
-            changePlayer();
+        if (cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
         }
     }
 
-    /**************** AFTER A WIN OR DRAW START THE GAME AGAIN ******************/
-    function restartGame(){
-        currentPlayer = "X";
-        choiceCells = ["", "", "", "", "", "", "", "", ""];
-        popUp.textContent = `${currentPlayer}'s turn`;
-        cells.forEach(cell => cell.textContent = "");
-        isGameActive = true;
+    if (roundWon) {
+        popUp.textContent = `${currentPlayer} wins!`;
+        isGameActive = false;
+    } else if (!choiceCells.includes("")) {
+        popUp.textContent = `Draw!`;
+        isGameActive = false;
+    } else {
+        changePlayer();
     }
-    
+}
+
+/**************** AFTER A WIN OR DRAW START THE GAME AGAIN ******************/
+function restartGame() {
+    currentPlayer = "X";
+    choiceCells = ["", "", "", "", "", "", "", "", ""];
+    popUp.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    isGameActive = true;
+}
